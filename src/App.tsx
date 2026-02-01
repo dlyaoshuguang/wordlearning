ab45332c6896c53d04545b3231d0b811e2a37bb4
dlyaoshuguang <yaoshuguang@hotmail.com>
2026-02-01 13:58:54 +0800

    追加了api调用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+interface Translation {
+  pos: string
+  tran_cn: string
+}
 function App() {
   const [count, setCount] = useState(0)
+  const [url, setUrl] = useState('https://v2.xxapi.cn/api/englishwords'+ '?word=' +'about');
+  const [translations, setTranslations] = useState<Translation[]>([])
 
-  return (
+  useEffect(() => {
+  axios.get(url)
+     .then(response => {
+      if(!response.data||response.data.length=== 0) {return; }
+      if(response.data.code!==200){
+        setTranslations([]);
+        return;
+      }
+      console.log(response.data.data);
+      setTranslations(response.data.data.translations);
+     })
+     .catch(error => {
+      alert("出错了！"+error.message);       
+     });
+ }, [url]);
+return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -20,6 +41,14 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <h1>
+          {translations.map((translation, index) => (
+            <div key={index}>
+              <p>{translation.pos}</p>
+              <p>{translation.tran_cn}</p>
+            </div>
+          ))}
+        </h1>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
